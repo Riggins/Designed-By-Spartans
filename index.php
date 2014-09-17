fcee8e90d5999bd160aaa325f73c024b23e8c35d
Nick Wittwer <nwdude@me.com>
2014-09-16 22:56:00 -0700

    NEW DESIGN

--- index.php
+++ index.php
@@ -4,101 +4,133 @@
 	<div class="row">
 		<div class="large-12 columns">
 			<nav>
-				<h1>Designed by Spartans</h1>
+			
+				<!-- account widget -->
+				<div class="account-widget">
+					<div class="account-image">
+						<img src="http://www.insidefacebook.com/wp-content/uploads/2013/01/profile-150x150.png" />
+					</div>
+					
+					<div class="account-profile">
+						<span>Profile ></span>
+					</div>
+				</div><!-- end account widget -->
+			
+			
+				<h4>Designed by Spartans</h4>
+				
+				<br />
 				
 				<ul>
 					<li>Home</li>
 					<li>Give Feedback</li>
 					<li>Search...</li>
 				</ul>
+				
+			
+				
+				<div class="clearfix"></div>
+				
 			</nav>
 		</div>
 	</div>
 	<!-- / End Navigation -->
 	
-	<hr />
 	
 	<!-- Start Content Work -->
 	<div class="row">
-			<div class="large-2 columns">
-				Sidebar here
+			
+			<div class="large-3 columns">
+				
+				<h6>Articles &amp; News</h6>
+				
+				<div class="blog-listing">
+					<div class="blog-inner">
+						<span class="title">Title</span>
+						<span class="text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</span>
+					</div>
+				</div>
+				
+				<div class="blog-listing">
+					<div class="blog-inner">
+						<span class="title">Title</span>
+						<span class="text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</span>
+					</div>
+				</div>
+				
+				<div class="blog-listing">
+					<div class="blog-inner">
+						<span class="title">Title</span>
+						<span class="text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</span>
+					</div>
+				</div>
+				
 			</div>
 	
-			<div class="large-10 columns">
+			<div class="large-6 columns">
 			<!-- Featured Work -->	
-				<h3>Featured Work</h3>
+				<h6>Featured Work</h6>
 				
-				<div class="large-3 columns">
-					<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-				</div>
-				<div class="large-3 columns">
-					<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-				</div>
-				<div class="large-3 columns">
-					<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-				</div>
-				<div class="large-3 columns">
-					<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-				</div>
+				<ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-3" id="home-gallery">
+				  <li><img class="th perk-up" src="https://d13yacurqjgara.cloudfront.net/users/1641/screenshots/1728826/ducati.gif"></li>
+				  <li><img class="th perk-up" src="https://d13yacurqjgara.cloudfront.net/users/13307/screenshots/1728095/line_chart.jpg"></li>
+				  <li><img class="th perk-up" src="https://d13yacurqjgara.cloudfront.net/users/43342/screenshots/1725663/oap.jpg"></li>
+				  <li><img class="th perk-up" src="https://d13yacurqjgara.cloudfront.net/users/108671/screenshots/1723082/infotimes.jpg"></li>
+				  <li><img class="th perk-up" src="https://d13yacurqjgara.cloudfront.net/users/107759/screenshots/1720359/apple-watch.png"></li>
+				  <li><img class="th perk-up" src="https://d13yacurqjgara.cloudfront.net/users/9745/screenshots/1720826/br-small.png"></li>
+				  <li><img class="th perk-up" src="https://d13yacurqjgara.cloudfront.net/users/43342/screenshots/1719521/studiojq2013_dashboard_all_shot.jpg"></li>
+				  <li><img class="th perk-up" src="https://d13yacurqjgara.cloudfront.net/users/13307/screenshots/1716815/steampunk.jpg"></li>
+				  <li><img class="th perk-up" src="https://d13yacurqjgara.cloudfront.net/users/329207/screenshots/1716533/dribble_1.jpg"></li>
+				</ul>
+			
+			</div>
 			<!-- / End Featured Work -->
+			
+			<div class="large-3 columns">
 				
-			<!-- Feedback -->
-			<div class="row">
-				<div class="large-10 columns">
-					<h3>Feedback</h3>
+				<h6>Upcoming</h6>
+				
+				<div class="calendar">
+				
+				
+					<div class="cal-item ">
+						<div class="cal-date">
+							<span>Aug. 25</span>
+						</div>
+						<div class="cal-title">
+							<span>Event Title</span>
+							<span class="cal-text">Something goes here</span>
+						</div>
+						<div class="clearfix"></div>
+					</div>
 					
-					<div class="large-3 columns homepage-box">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
+					<div class="cal-item">
+						<div class="cal-date">
+							<span>Aug. 25</span>
+						</div>
+						<div class="cal-title">
+							<span>Event Title</span>
+							<span class="cal-text">Something goes here</span>
+						</div>
+						<div class="clearfix"></div>
 					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
+					
+					<div class="cal-item">
+						<div class="cal-date">
+							<span>Aug. 25</span>
+						</div>
+						<div class="cal-title">
+							<span>Event Title</span>
+							<span class="cal-text">Something goes here</span>
+						</div>
+						<div class="clearfix"></div>
 					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-					</div>
-					<div class="large-3 columns">
-						Hi
-						<img src="http://upload.wikimedia.org/wikipedia/commons/6/6a/Koala-ag1.jpg"/>
-					</div>
+					
+					
 				</div>
 			</div>
-			<!-- / End Feedback -->
-	</div>
-	<!-- / End Content -->
+			
+	</div><!-- End Content work -->
+				
  
 <?php get_footer(); ?>
